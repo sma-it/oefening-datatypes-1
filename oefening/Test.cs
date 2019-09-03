95c9ff81c5822d1d0575ecff032344df9ae054a8
yvanvds <yvan@attr-x.net>
2019-09-03 11:29:21 +0200

    updated test descriptions

--- oefening/Test.cs
+++ oefening/Test.cs
@@ -7,43 +7,43 @@ namespace Tests
 	[TestFixture]
 	public class TestOef1
 	{
-		[Test]
+		[Test, Description("Oef 1.1 - InitInt1")]
 		public void Test01_InitInt1()
 		{
 			Assert.That(First.Oef1.InitInt1(), Is.EqualTo(7), "3: Het getal is niet gelijk aan 7");
 		}
 
-		[Test]
+		[Test, Description("Oef 1.2 - InitInt2")]
 		public void Test02_InitInt2()
 		{
 			Assert.That(First.Oef1.InitInt2(), Is.EqualTo(-1), "3: Deze som klopt niet");
 		}
 
-		[Test] 
+		[Test, Description("Oef 1.3 - InitFloat")] 
 		public void Test03_InitFloat()
 		{
 			Assert.That(First.Oef1.InitFloat(), Is.EqualTo(3.14), "3: Deze float is niet gelijk aan 3.14");
 		}
 
-		[Test]
+		[Test, Description("Oef 1.4 - Prijs")]
 		public void Test04_Prijs()
 		{
 			Assert.That(First.Oef1.Prijs(), Is.EqualTo(1.99), "3: Dat is niet de juiste prijs");
 		}
 
-		[Test]
+		[Test, Description("Oef 1.5 - InitNaam")]
 		public void Test05_InitNaam()
 		{
 			Assert.That(First.Oef1.InitNaam(), Is.EqualTo("Hector"), "3: De naam is niet 'Hector'");
 		}
 
-		[Test]
+		[Test, Description("Oef 1.6 - InitStad")]
 		public void Test06_InitStad()
 		{
 			Assert.That(First.Oef1.InitStad(), Is.EqualTo("Aarschot"), "3: De stad is niet 'Aarschot'");
 		}
 
-		[Test]
+		[Test, Description("Oef 1.7 - InitChar")]
 		public void Test07_InitChar()
 		{
 			Assert.That(First.Oef1.InitChar(), Is.EqualTo('a'), "3: Het resultaat moet de letter a zijn");
@@ -54,83 +54,84 @@ namespace Tests
 	[TestFixture]
 	public class TestOef2
 	{
-		[Test]
-		public void Test_Som1()
+		[Test, Description("Oef 2.1 - Som1")]
+		public void Test01_Som1()
 		{
-			Assert.That(First.Oef2.Som1(50, 23), Is.EqualTo(73));
-			Assert.That(First.Oef2.Som1(12, -34), Is.EqualTo(-22));
+			Assert.That(First.Oef2.Som1(50, 23), Is.EqualTo(73), "5: Je som is niet correct");
+			Assert.That(First.Oef2.Som1(12, -34), Is.EqualTo(-22), "5: Je som is niet correct");
 		}
 
-		[Test]
-		public void Test_Som2()
+		[Test, Description("Oef 2.2 - Som2")]
+		public void Test02_Som2()
 		{
-			Assert.That(First.Oef2.Som2(3.1F, 5.4F), Is.EqualTo(8.5F));
-			Assert.That(First.Oef2.Som2(1.0F, -2.1F), Is.EqualTo(-1.1F));
+			Assert.That(First.Oef2.Som2(3.1F, 5.4F), Is.EqualTo(8.5F), "5: Je som is niet correct");
+			Assert.That(First.Oef2.Som2(1.0F, -2.1F), Is.EqualTo(-1.1F), "5: Je som is niet correct");
 		}
 
-		[Test]
-		public void Test_Som3()
+		[Test, Description("Oef 2.3 - Som3")]
+		public void Test03_Som3()
 		{
-			Assert.That(First.Oef2.Som3(3, 5, 0, -7), Is.EqualTo(1));
-			Assert.That(First.Oef2.Som3(100, 22, -9, 9), Is.EqualTo(122));
+			Assert.That(First.Oef2.Som3(3, 5, 0, -7), Is.EqualTo(1), "5: Je som is niet correct");
+			Assert.That(First.Oef2.Som3(100, 22, -9, 9), Is.EqualTo(122), "5: Je som is niet correct");
 		}
 
-		[Test]
-		public void Test_Naam1()
+		[Test, Description("Oef 2.4 - Naam1")]
+		public void Test04_Naam1()
 		{
-			Assert.That(First.Oef2.Naam1("Test", "Name"), Is.EqualTo("TestName"));
-			Assert.That(First.Oef2.Naam1("Abc", "Xyz"), Is.EqualTo("AbcXyz"));
+			Assert.That(First.Oef2.Naam1("Test", "Name"), Is.EqualTo("TestName"), "5: Beide argumenten moeten samen het resultaat vormen");
+			Assert.That(First.Oef2.Naam1("Abc", "Xyz"), Is.EqualTo("AbcXyz"), "5: Beide argumenten moeten samen het resultaat vormen");
 		}
 		
-		[Test]
-		public void Test_Naam2()
+		[Test, Description("Oef 2.5 - Naam2")]
+		public void Test05_Naam2()
 		{
-			Assert.That(First.Oef2.Naam2("Test", "Name"), Is.EqualTo("Test Name"));
-			Assert.That(First.Oef2.Naam2("Abc", "Xyz"), Is.EqualTo("Abc Xyz"));
+			Assert.That(First.Oef2.Naam2("Test", "Name"), Is.EqualTo("Test Name"), "5: Beide argumenten moeten samen het resultaat vormen, met een spatie er tussen");
+			Assert.That(First.Oef2.Naam2("Abc", "Xyz"), Is.EqualTo("Abc Xyz"), "5: Beide argumenten moeten samen het resultaat vormen, met een spatie er tussen");
 		}
 	}
 
+    // derde oefening staat op 25 punten
 	[TestFixture]
 	public class TestOef3
 	{
-		[Test]
+		[Test, Description("Oef 3.1 - Convert1")]
 		public void Test_Convert1()
 		{
-			Assert.That(First.Oef3.Convert1(7), Is.EqualTo(7.0F));
-			Assert.That(First.Oef3.Convert1(4), Is.EqualTo(4.0F));
-			Assert.That(First.Oef3.Convert1(3).GetType() == typeof(float), Is.EqualTo(true));
+			Assert.That(First.Oef3.Convert1(7), Is.EqualTo(7.0F), "5: Dit is geen correcte float conversie");
+			Assert.That(First.Oef3.Convert1(4), Is.EqualTo(4.0F), "5: Dit is geen correcte float conversie");
+			Assert.That(First.Oef3.Convert1(3).GetType() == typeof(float), Is.EqualTo(true), "3: De waarde lijkt correct, maar het is geen float");
 		}
 
-		[Test]
+		[Test, Description("Oef 3.2 - Convert2")]
 		public void Test_Convert2()
 		{
-			Assert.That(First.Oef3.Convert2(7.3F), Is.EqualTo(7));
-			Assert.That(First.Oef3.Convert2(-1.2F), Is.EqualTo(-1));
-			Assert.That(First.Oef3.Convert2(3F).GetType() == typeof(int), Is.EqualTo(true));
+			Assert.That(First.Oef3.Convert2(7.3F), Is.EqualTo(7), "5: Dit is geen correcte integer conversie");
+			Assert.That(First.Oef3.Convert2(-1.2F), Is.EqualTo(-1), "5: Dit is geen correcte integer conversie");
+			Assert.That(First.Oef3.Convert2(3F).GetType() == typeof(int), Is.EqualTo(true), "3: De waarde lijkt correct, maar het is geen integer");
 		}
 
-		[Test]
+		[Test, Description("Oef 3.3 - Som1")]
 		public void Test_Som1()
 		{
-			Assert.That(First.Oef3.Som1(2.3F, 4), Is.EqualTo(6.3F));
-			Assert.That(First.Oef3.Som1(4.5F, -6), Is.EqualTo(-1.5F));
-			Assert.That(First.Oef3.Som1(4.2F, 1).GetType() == typeof(float), Is.EqualTo(true));
+			Assert.That(First.Oef3.Som1(2.3F, 4), Is.EqualTo(6.3F), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som1(4.5F, -6), Is.EqualTo(-1.5F), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som1(4.2F, 1).GetType() == typeof(float), Is.EqualTo(true), "3: De som is juist, maar het resultaat moet een float zijn");
 		}
 
-		[Test]
+		[Test, Description("Oef 3.4 - Som2")]
 		public void Test_Som2()
 		{
-			Assert.That(First.Oef3.Som2(2.3F, 4), Is.EqualTo(6));
-			Assert.That(First.Oef3.Som2(4.5F, -6), Is.EqualTo(-1));
-			Assert.That(First.Oef3.Som2(4.2F, 1).GetType() == typeof(int), Is.EqualTo(true));
+			Assert.That(First.Oef3.Som2(2.3F, 4), Is.EqualTo(6), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som2(4.5F, -6), Is.EqualTo(-1), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som2(4.2F, 1).GetType() == typeof(int), Is.EqualTo(true), "3: De som is juist, maar het resultaat moet een integer zijn");
 		}
 
-		[Test]
+		[Test, Description("Oef 3.5 - Som3")]
 		public void Test_Som3()
 		{
-			Assert.That(First.Oef3.Som3(2.3, 4.3), Is.EqualTo(6.6F));
-			Assert.That(First.Oef3.Som3(13, 0.1), Is.EqualTo(13.1F));
-			Assert.That(First.Oef3.Som3(4.2F, 1).GetType() == typeof(float), Is.EqualTo(true));
+			Assert.That(First.Oef3.Som3(2.3, 4.3), Is.EqualTo(6.6F), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som3(13, 0.1), Is.EqualTo(13.1F), "5: De som is niet juist");
+			Assert.That(First.Oef3.Som3(4.2F, 1).GetType() == typeof(float), Is.EqualTo(true), "3: De som is juist, maar het resultaat moet een float zijn");
 		}
 	}
 
